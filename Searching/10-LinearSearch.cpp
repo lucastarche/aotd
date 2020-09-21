75adfceebb358e7388e738ec2fac55b571fb269e
Lucas Hernán Tarche <lucastarche@gmail.com>
2020-09-21 11:08:31 -0300

    Day 10: Linear Search

---
+++ Searching/10-LinearSearch.cpp
@@ -0,0 +1,26 @@
+//Linear Search
+//Iterate through each element of the array, until you find the element wanted or run out of elements.
+//Best case complexity: O(1)
+//Average case complexity: O(n)
+//Worst case complexity: O(n)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool LinearSearch(const vector<int> &v, int val) {
+    for (int i = 0; i < (int)v.size(); i++) {
+        if (v[i] == val) return true;
+    }
+    return false;
+}
+
+int main() {
+    int n, val;
+    cin >> n;
+    vector<int> elements(n);
+    for (int i = 0; i < n; i++) cin >> elements[i];
+    cin >> val;
+    bool found = LinearSearch(elements, val);
+    if (found) cout << val << " is in the array.\n";
+    else cout << val << " is not in the array.\n";
+}
