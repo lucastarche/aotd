103e0fd7a48c92f2b87f4faa547537d76a9f2c7d
Lucas Hernán Tarche <lucastarche@gmail.com>
2020-10-05 15:37:50 -0300

    Day 24: DFS

---
+++ Graphs/24-DFS.cpp
@@ -0,0 +1,43 @@
+//Depth First Search (DFS)
+//It starts at a certain node, and gets the deepest possible in the graph, before backtracking to earlier unvisited edges.
+//Runtime: O(n^2) with Adjacency Matrix, O(n + m) with Adjacency List
+//Interesting applications: maze traversal, topological sorting, checking graph connectivity, and much more.
+//This is the iterative version, but there is also the (classical) recursive version, which works in a similar way.
+#include "GraphAL.cpp"
+
+vector<bool> DFS(GraphAL &g, int start) {
+    stack<int> s;
+    vector<bool> visited(g.getSize() + 1);
+    s.push(start);
+
+    while (!s.empty()) {
+        int curr = s.top();
+        s.pop();
+        if (visited[curr]) continue;
+        visited[curr] = true;
+        for (auto node : g.getNeighbours(curr)) {
+            s.push(node);
+        }
+    }
+
+    return visited;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    GraphAL graph(n);
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        graph.addEdge(a, b);
+    }
+
+    int source;
+    cin >> source;
+    vector<bool> ans = DFS(graph, source);
+    for (int i = 1; i <= n; i++) {
+        if (ans[i]) cout << i << " is reachable from " << source << '\n';
+        else cout << i << " is not reachable from " << source << '\n';
+    }
+}
---
+++ Graphs/GraphAL.cpp
@@ -0,0 +1,40 @@
+//Note: This is a copy of aotd#23, but without defining main() to use it in other files.
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class GraphAL {
+    private:
+        vector<vector<int>> ady;
+        int size;
+    public:
+        GraphAL(int n) {
+            ady.resize(n + 1);
+            size = n;
+        }
+
+        void addEdge(int a, int b) {
+            ady[a].push_back(b);
+            ady[b].push_back(a);
+        }
+
+        void addDirectedEdge(int from, int to) {
+            ady[from].push_back(to);
+        }
+
+        bool connected(int from, int to) {
+            for (auto a : ady[from]) {
+                if (a == to) return true;
+            }
+            return false;
+        }
+
+        int getSize() {
+            return size;
+        }
+
+        vector<int> getNeighbours(int n) {
+            return ady[n];
+        }
+};
+
