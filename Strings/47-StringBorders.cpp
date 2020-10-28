9fc5ec1f5c9a30c8a654fcdea4716ffc278fc56d
Lucas Hernán Tarche <lucastarche@gmail.com>
2020-10-28 15:03:38 -0300

    Day 47: String Borders
    Fixed typo

--- Strings/47-StringBorders.cpp
+++ Strings/47-StringBorders.cpp
@@ -1,5 +1,5 @@
 //String Borders
-//Determines the longest substring that isn't the entire string, such that it is both a prefix (ab is a prefix of abcde) and a suffix (de is a prefix of abcde).
+//Determines the longest substring that isn't the entire string, such that it is both a prefix (ab is a prefix of abcde) and a suffix (de is a suffix of abcde).
 //This little algorithm will help when we implement Knuth-Morris-Pratt, since it is a subproblem needed.
 //Runtime: O(n).
 #include <bits/stdc++.h>
