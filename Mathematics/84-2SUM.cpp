8e5af781dbc735c4a6ed3de30e97e96bd4d54abf
lucastarche <lucastarche@gmail.com>
2020-12-04 11:32:54 -0300

    Day 84: 2-SUM

---
+++ Mathematics/84-2SUM.cpp
@@ -0,0 +1,35 @@
+//2-SUM
+//Determines whether there are exactly two numbers in an array whose sum results in a given number x.
+//The naive algorithm runs in O(n^2) by trying every possible two values.
+//We can make this much faster by sorting the array first, and used a method known as Two Pointers.
+//Runtime: O(n log n).
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool TwoSUM(const vector<int> &nums, int target) {
+    int n = (int)nums.size();
+
+    int lptr = 0, rptr = n - 1;
+    for (int sum = nums[lptr] + nums[rptr]; sum != target; sum = nums[lptr] + nums[rptr]) {
+        if (lptr >= rptr) break;
+        if (sum > target) {
+            rptr--;
+        }
+        else {
+            lptr++;
+        }
+    }
+
+    if (nums[lptr] + nums[rptr] == target) return true;
+    return false;
+}
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
+
+    cout << TwoSUM(nums, x) << '\n';
+}
