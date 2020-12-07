a75d3d10f0a4ec998c6e269bf464eb63a0dc37e1
lucastarche <lucastarche@gmail.com>
2020-12-07 11:12:54 -0300

    Day 87: Successor Graph

---
+++ Graphs/87-SuccessorGraph.cpp
@@ -0,0 +1,39 @@
+//Succesor Graph
+//Also known as a functional graph, or surjection graph.
+//Each node only has one successor (also noted as f(x)), but can have more than one parent.
+//Runtime: building runs in O(n).
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SuccessorGraph {
+    private:
+        vector<int> next;
+    public:
+        SuccessorGraph(vector<int> next) : next{next} { }
+
+        int succ(int x) {
+            return next[x];
+        }
+
+        int size() {
+            return (int)next.size();
+        }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> next(n);
+    for (int i = 0; i < n; i++) cin >> next[i];
+
+    SuccessorGraph g(next);
+
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int a;
+        cin >> a;
+        cout << g.succ(a) << '\n';
+    }
+}
