7f6a658fda95008281b1f1fd4038a801ceee365d
lucastarche <lucastarche@gmail.com>
2020-12-24 15:13:36 -0300

    Day 104: Christmas Tree

---
+++ Misc/104-ChristmasTree.cpp
@@ -0,0 +1,27 @@
+//Christmas tree
+//Shows a Christmas tree :D
+//Runtime: O(n), unless modificating a string runs in O(n), in which case it is O(n^2).
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void ChristmasTree(int levels) {
+    string separator = "", branches = "*";
+    for (int i = 0; i < levels; i++) 
+        separator += " ";
+
+    for (int i = 0; i < levels; i++) {
+        cout << separator << branches << '\n';
+        separator = separator.substr(1);
+        branches += "**";
+    }
+
+    for (int i = 0; i < levels; i++) cout << ' ';
+    cout << "|\n";
+}
+
+int main() {
+    int n;
+    cin >> n;
+    ChristmasTree(n);
+}
