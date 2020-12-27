6e6fa48045779298bb001c3251f46774263c082d
lucastarche <lucastarche@gmail.com>
2020-12-27 15:00:53 -0300

    Day 107: Red Black Tree Rotations

---
+++ DataStructures/RedBlackTree/107-Rotations.cpp
@@ -0,0 +1,51 @@
+//Red Black Tree: Rotations
+//The left and right rotations are two constant operations, which are used to balance the red black tree.
+//This operations have the property that the Binary-Search property of the tree is mantained.
+//Runtime: O(1)
+#include "106-ConstructorAndColouring.cpp"
+
+template <typename T>
+void RBTree::leftRotation(Node<T> *src) {
+    if (src->right == null) return;
+    Node<T> *x = src;
+    Node<T> *y = src->right;
+
+    //Make y's left subtree into x's right subtree
+    x->right = y->left; 
+    if (y->left != null) y->left->parent = x;
+
+    //Fix y's parent
+    y->parent = x->parent;
+
+    //Fix x's parent
+    if (x->parent == null) root = y;
+    else if (x == x->parent->left) x->parent->left = y;
+    else x->parent->right = y;
+
+    //Put x to the left of y
+    y->left = x;
+    x->parent = y;
+}
+
+template <typename T>
+void RBTree::rightRotation(Node<T> *src) {
+    if (src->left == null) return;
+    Node<T> *x = src;
+    Node<T> *y = src->left;
+
+    //Make y's right subtree into x's left subtree
+    x->left = y->right; 
+    if (y->right != null) y->right->parent = x;
+
+    //Fix y's parent
+    y->parent = x->parent;
+
+    //Fix x's parent
+    if (x->parent == null) root = y;
+    else if (x == x->parent->left) x->parent->left = y;
+    else x->parent->right = y;
+
+    //Put x to the left of y
+    y->right = x;
+    x->parent = y;
+}
