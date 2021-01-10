71af8e95c46942ea14df6d73daecd5a5e5283278
lucastarche <lucastarche@gmail.com>
2021-01-10 16:24:23 -0300

    Day 121: Coctail Shaker Sort

---
+++ Sorting/121-CoctailShakerSort.cpp
@@ -0,0 +1,42 @@
+//Coctail Shaker Sort
+//A constant time improvement over bubble sort, but that still pales with large arrays.
+//Despite being (on average) twice as fast as bubble sort, it still runs in quadratic time.
+//Runtime: O(n^2)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void CoctailShakerSort(vector<int> &arr) {
+    int n = (int)arr.size();
+    bool swapped = true;
+    while (swapped) {
+        swapped = false;
+        
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n, 0);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    CoctailShakerSort(arr);
+
+    for (auto a : arr) cout << a << ' ';
+    cout << '\n';
+}
