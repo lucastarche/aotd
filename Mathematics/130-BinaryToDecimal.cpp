1f09bade59e2d6e83fa54b3eb1683087e927440b
lucastarche <lucastarche@gmail.com>
2021-01-19 15:31:24 -0300

    Day 130: Binary to Decimal

---
+++ Mathematics/130-BinaryToDecimal.cpp
@@ -0,0 +1,24 @@
+//Binary To Decimal
+//We can convert a number from binary to decimal by multiplying each number with 2^(position - 1)
+//Runtime: O(log n), where n is the number in decimal.
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int BinaryToDecimal(string binaryString) {
+    int n = (int)binaryString.size();
+    int ans = 0, pow = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        if (binaryString[i] == '1') {
+            ans += 1<<pow;
+        }
+        pow++;
+    }
+    return ans;
+}
+
+int main() {
+    string n;
+    cin >> n;
+    cout << BinaryToDecimal(n) << '\n';
+}
