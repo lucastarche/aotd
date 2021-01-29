a07f92b5b431065b2a2da26a2d2df5ec6b764bfa
lucastarche <lucastarche@gmail.com>
2021-01-29 17:26:44 -0300

    Day 140: ProjectEuler 4

---
+++ ProjectEuler/140-PE4-LargestPalindromeProduct.cpp
@@ -0,0 +1,24 @@
+//PE4: Largest Palindrome Product
+//Problem statement: find the largest product of two three-digit numbers, which is also a palindrome.
+//Solution: We can simply iterate through every possible combination for both numbers.
+//For this, we will create a function to add to the utilities header, which is called is_palindrome()
+//Runtime: O(n^2 * log(n))
+#include "EulerUtils.hpp"
+
+long long solve() {
+    long long ans = 0LL;
+    
+    for (long long i = 100; i <= 999; i++) {
+        for (long long j = i; j <= 999; j++) {
+            if (is_palindrome(i * j)) {
+                ans = max(ans, i * j);
+            }
+        }
+    }
+    
+    return ans;
+}
+
+int main() {
+    cout << solve() << '\n';
+}
--- ProjectEuler/EulerUtils.hpp
+++ ProjectEuler/EulerUtils.hpp
@@ -15,3 +15,13 @@ set<long long> prime_factors(long long n) {
 
     return ans;
 }
+
+bool is_palindrome(string str) {
+    string copy;
+    reverse_copy(str.begin(), str.end(), copy);
+    return str == copy;
+}
+
+bool is_palindrome(long long n) {
+    return is_palindrome(to_string(n));
+}
